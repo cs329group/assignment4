8c3739109734afe905d4244d0942ce4c8c1d73d9
Mason Berhenke <stringsanbu@gmail.com>
2015-10-15 11:46:52 -0500

    Adding initial project

---
+++ assignment4/assignment4.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace assignment4
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			// Initial checks before we begin the application
+			 
+			if(args.Length == 0)
+		}
+	}
+}
