20aadebc54d24cc847754463e4756b38a613f364
mamckill <mamckill@iastate.edu>
2015-10-19 16:34:38 -0500

    * Job.cs: * PDEalgorithm.cs: * assignment4.csproj: * assignment4.exe: * assignment4.exe:
    * assignment4.cs: I have created a Job class and also the pde
      algorithm. There are still some errors, but its close.

---
+++ assignment4/Job.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace assignment4
+{
+	public class Job
+	{
+		public string name { get; set; }
+		public int duration { get; set; }
+		public List<Job> predecessors { get; set; }
+		public List<Job> successors { get; set; }
+		public int earlyStart { get; set; }
+		public int earlyEnd { get; set; }
+		public int lateStart { get; set; }
+		public int lateEnd { get; set; }
+
+		public Job(string name, int duration, List<Job> predecessors, List<Job> successors)
+		{
+			this.name = name;
+			this.duration = duration;
+			this.predecessors = predecessors;
+			this.successors = predecessors;
+		}
+
+		public void setSuccessors(List<Job> successors){
+			this.successors = successors;
+		}
+
+		public void setPredecessors(List<Job> predecessors){
+			this.predecessors = predecessors;
+		}
+	}
+}
+
---
+++ assignment4/PDEalgorithm.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace assignment4
+{
+	public class PDEalgorithm
+	{
+		List<Job> schedule;
+		public PDEalgorithm (List<Job> schedule)
+		{
+			this.schedule = schedule;
+			int scheduleEnd = fillEarliest (schedule);
+			fillLatest (schedule, scheduleEnd);
+			foreach (Job job in schedule) {
+				Console.Write ("Name:" + job.name + "   Duration:" + job.duration + "   ES:" + job.earlyStart + "   EE:" + job.earlyEnd + "   LS:" + job.lateStart + "   LS:" + job.lateEnd+"\n");
+			}
+		}
+
+		public int fillEarliest(List<Job> schedule)
+		{
+			int scheduleEnd = 0;
+			Queue<Job> jobQueue = new Queue<Job> ();
+
+			// push all jobs that are at the beginning (no predecessors)
+			foreach (Job job in schedule) {
+				if (job.predecessors.Count == 0) {
+					jobQueue.Enqueue (job);
+				}
+			}
+
+			// BFS through job schedule
+			while (jobQueue.Count != 0) {
+				Job curJob = jobQueue.Dequeue ();
+				// Console.Write ("1\n");
+				// the job is at the begninning of the schedule
+				if (curJob.predecessors.Count == 0) {
+					curJob.earlyStart = 0;
+				} else {
+					int maxEarliestEning = 0;
+					// find the predecessor with the greatest earliest ending, which will serve as the curJob's earliest start
+					foreach (Job prevJob in curJob.predecessors) {
+						if (prevJob.earlyEnd> maxEarliestEning) {
+							maxEarliestEning = prevJob.earlyEnd;
+						}
+					}
+					curJob.earlyStart = maxEarliestEning;
+				}
+
+				curJob.earlyEnd = curJob.earlyStart + curJob.duration;
+				if (scheduleEnd < curJob.earlyEnd) {
+					scheduleEnd = curJob.earlyEnd;
+				}
+
+				// push the curjob's successors onto the stack
+				// Console.Write ("Name: " + curJob.name + " successors count: " + curJob.successors.Count + "\n");
+				foreach (Job nextJob in curJob.successors) {
+					// Console.Write ("curJob: " + curJob.name + " succ: " + nextJob.name + "\n");
+					jobQueue.Enqueue (nextJob);
+				}
+			}
+			return scheduleEnd;
+		}
+
+		public void fillLatest(List<Job> schedule, int scheduleEnd){
+			Queue<Job> jobQueue = new Queue<Job> ();
+
+			// push all jobs that are at the beginning (no predecessors)
+			foreach (Job job in schedule) {
+				if (job.successors.Count == 0) {
+					jobQueue.Enqueue (job);
+				}
+			}
+
+			// BFS through job schedule
+			while (jobQueue.Count != 0) {
+				Job curJob = jobQueue.Dequeue ();
+
+				// the job is at the end of the schedule
+				if (curJob.successors.Count == 0) {
+					curJob.lateEnd = scheduleEnd;
+				} else {
+					int minLatestStart = 0;
+					// find the successor with the smallest latest start, which will serve as the curJob's latest end
+					foreach (Job nextJob in curJob.successors) {
+						if (nextJob.lateStart < minLatestStart) {
+							minLatestStart = nextJob.lateStart;
+						}
+					}
+
+					curJob.lateEnd = minLatestStart;
+				}
+
+				// push the curjob's successors onto the stack
+				foreach (Job prevJob in curJob.predecessors) {
+					jobQueue.Enqueue (prevJob);
+				}
+				curJob.lateStart = curJob.lateEnd - curJob.duration;
+			}
+		}
+	}
+}
+
